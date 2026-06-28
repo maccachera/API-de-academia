09ccf6241dadaf97344781957bceab415f3f6dc6
Gabriel Maccachero <maccacherog@gmail.com>
2026-06-28 14:27:02 -0300

    Criação das views

--- academia/views.py
+++ academia/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Unidade, Plano, Aluno, FichaTreino, RegistroAcesso
+from .serializers import (
+    UnidadeSerializer, PlanoSerializer, AlunoSerializer, 
+    FichaTreinoSerializer, RegistroAcessoSerializer
+)
 
-# Create your views here.
+class UnidadeViewSet(viewsets.ModelViewSet):
+    queryset = Unidade.objects.all()
+    serializer_class = UnidadeSerializer
+
+class PlanoViewSet(viewsets.ModelViewSet):
+    queryset = Plano.objects.all()
+    serializer_class = PlanoSerializer
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class FichaTreinoViewSet(viewsets.ModelViewSet):
+    queryset = FichaTreino.objects.all()
+    serializer_class = FichaTreinoSerializer
+
+class RegistroAcessoViewSet(viewsets.ModelViewSet):
+    queryset = RegistroAcesso.objects.all()
+    serializer_class = RegistroAcessoSerializer
