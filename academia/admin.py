5054a4b5ec07bcaf853b307b7c5b409c28dd9df2
Gabriel Maccachero <maccacherog@gmail.com>
2026-06-28 13:55:47 -0300

    Criação dos superusuarios, e registro no admin

--- academia/admin.py
+++ academia/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from django.contrib import admin
+from .models import Aluno, Unidade, Plano, FichaTreino, RegistroAcesso
+
+admin.site.register(Aluno)
+admin.site.register(Unidade)
+admin.site.register(Plano)
+admin.site.register(FichaTreino)
+admin.site.register(RegistroAcesso)
